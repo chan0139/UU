89a67e01f9ad06c02580b80a424078622eb0f3a8
MK <simon365@naver.com>
2021-10-02 02:27:13 +0900

    Kakao Map connection
    fatal bug -map screen does not appear in device. maybe because of key hash mismatch

--- app/src/main/java/com/example/uu/MainActivity.java
+++ app/src/main/java/com/example/uu/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
+
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     TextView title;
@@ -62,7 +64,33 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
         hideNavigationBar();
+
     }
+/*  to get a signature Key for the app, if want to confirm the key, call 'geyHashKey' func in onCreate and check the log
+
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
+ */
+
     private void hideNavigationBar() {
         int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
         int newUiOptions = uiOptions;
--- app/src/main/java/com/example/uu/fragment_running.java
+++ app/src/main/java/com/example/uu/fragment_running.java
@@ -9,11 +9,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import net.daum.mf.map.api.MapView;
+
 public class fragment_running extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootview=(ViewGroup) inflater.inflate(R.layout.fragment_running,container,false);
+
+        MapView mapView = new MapView((MainActivity)getActivity());
+
+        ViewGroup mapViewContainer = (ViewGroup)rootview.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
 
         return rootview;
     }
