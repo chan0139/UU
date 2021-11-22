0630938af23862d48ed51fed2bf8a6cfd4ecf418
MK <simon365@naver.com>
2021-11-22 10:06:19 +0900

    Connect data with circular progress bar
    user can now make their own acievement by circular progress bar.
    -distance
    -time
    -recruitment
    -and rest
    
    once user makes his own goal,
    data is automatically stored&loaded
    by local db

--- app/src/main/java/com/example/uu/fragment_record_results.java
+++ app/src/main/java/com/example/uu/fragment_record_results.java
@@ -1,5 +1,10 @@
 package com.example.uu;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
@@ -11,6 +16,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
@@ -45,27 +54,39 @@ public class fragment_record_results extends Fragment {
     //for circular progress bar
     private float progress;
     private String achievementName;
+    CircularProgressView personalAchievement;
+    TextView achievementText;
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
-    private int show_what;
+    int total_goal;
+    int current_state;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup rootView=(ViewGroup) inflater.inflate(R.layout.fragment_record_results, container, false);
 
-        //init
+        //init local db
         dbHelper=new DatabaseHelper(getContext());
         sqLiteDb=dbHelper.getReadableDatabase();
+        preferences= getActivity().getSharedPreferences("achievement", Activity.MODE_PRIVATE);
+        editor=preferences.edit();
 
 
         //CircularProgressView
-        show_what=1;
-        CircularProgressView personalAchievement=(CircularProgressView) rootView.findViewById(R.id.personalAchievement);
-        personalAchievement.setText("gi");
+        personalAchievement=(CircularProgressView) rootView.findViewById(R.id.personalAchievement);
+        achievementText=rootView.findViewById(R.id.achievementText);
+
+        //기존 설정했던 업적 보여주기
+        setAchievement();
+
+        //나만의 업적 재설정
         personalAchievement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setAchievement();
+                resetAchievement();
             }
         });
 
@@ -118,8 +139,7 @@ public class fragment_record_results extends Fragment {
 
     }
 
-    private void getRecordData()
-    {
+    private void getRecordData() {
         Cursor cursor;
 
         String day;
@@ -144,9 +164,6 @@ public class fragment_record_results extends Fragment {
             barEntries.add(new BarEntry(i+7,distance+5));
             xAxisName.add(day);
         }
-
-
-        sqLiteDb.close();
     }
 
     private static String getDateDay(int dayNum)  {
@@ -182,7 +199,134 @@ public class fragment_record_results extends Fragment {
         return day;
     }
 
-    private void setAchievement(){
+    private void setAchievement(){      //이미 저장해둔 지표가 있으면 가져와서 반영
 
+        if((preferences!=null)&&preferences.contains("goalObj")){
+            int goalIndex=preferences.getInt("goalObj",0);
+            int goal=preferences.getInt("goal",0);
+
+            updateCircularView(goalIndex,goal);
+        }
+    }
+
+    private void resetAchievement(){    //사용자가 개인 목표를 다시 설정할경우, 그에 맞춰 최신화
+        AlertDialog.Builder dlg = new AlertDialog.Builder(getContext());
+        dlg.setTitle("어떤 목표를 향해 달려볼까요?");
+        final String[] versionArray = new String[] {"많이 뛰고 싶어요!","오래 달리고 싶어요!","함께 운동하고 싶어요!","오늘은 쉬고싶어요."};
+        dlg.setIcon(R.drawable.ic_trophy_selected); // 아이콘 설정
+
+        dlg.setItems(versionArray, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                drawDialog(which);
+            }
+        });
+
+        dlg.setPositiveButton("취소",new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        dlg.show();
+    }
+
+    private void drawDialog(int index){
+        Dialog dialog=new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_setachievement);
+
+        TextView title=dialog.findViewById(R.id.title);
+        SeekBar setGoal=dialog.findViewById(R.id.setGoal);
+        TextView goalMin=dialog.findViewById(R.id.goalMin);
+        TextView goalMax=dialog.findViewById(R.id.goalMax);
+        Button setBtn=dialog.findViewById(R.id.setBtn);
+
+        switch (index){
+            case 0:
+                title.setText("얼만큼 달리고 싶으세요?");
+                setGoal.setProgress(0);
+                setGoal.setMax(9);
+                goalMin.setText("1km");
+                goalMax.setText("10km");
+                break;
+            case 1:
+                title.setText("얼마동안 운동 해볼까요?");
+                setGoal.setProgress(0);
+                setGoal.setMax(7);
+                goalMin.setText("30min");
+                goalMax.setText("240min");
+                break;
+            case 2:
+                title.setText("몇번의 모집에 참가해볼까요?");
+                setGoal.setProgress(0);
+                setGoal.setMax(4);
+                goalMin.setText("1");
+                goalMax.setText("5");
+                break;
+            default:
+                editor.clear();
+                editor.putInt("goalObj",index);
+                editor.commit();
+                updateCircularView(index,0);
+                return;
+
+        }
+        dialog.show();
+
+        setBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int userGoal=setGoal.getProgress()+1;
+                int goalIndex=index;
+
+                editor.clear();
+                editor.putInt("goalObj",goalIndex);
+                editor.putInt("goal",userGoal);
+                editor.commit();
+
+                updateCircularView(index,userGoal);
+
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void updateCircularView(int index,int goal){
+        String[] objList=new String[] {"Km\n달리기!","분 동안\n운동하기!","회 모집\n참가하기!"};
+        Cursor cursor;
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE , 0);
+        String today = new java.text.SimpleDateFormat("yyyy:MM:dd%").format(calendar.getTime());
+        String targetTable="";
+
+        switch (index){
+            case 0:
+                achievementText.setText(Integer.toString(goal)+objList[index]);
+                total_goal=goal*1000;
+                targetTable=DatabaseHelper.RUNNING_DISTANCE;
+                break;
+            case 1:
+                achievementText.setText(Integer.toString((goal)*30)+objList[index]);
+                total_goal=goal*30;
+                targetTable=DatabaseHelper.RUNNING_TIME;
+                break;
+            case 2:
+                achievementText.setText(Integer.toString(goal)+objList[index]);
+                total_goal=goal;
+                break;
+            default:
+                achievementText.setText("재충전은\n필수라구요!!Zzz");
+                personalAchievement.setProgress(100);
+                break;
+        }
+
+        if(index==0 || index==1) {
+            String queryDistanceSum = "SELECT SUM(" + targetTable + ") FROM " + DatabaseHelper.TABLE_NAME + " WHERE " + DatabaseHelper.PRIMARY_KEY + " LIKE '" + today + "';";
+            cursor = sqLiteDb.rawQuery(queryDistanceSum, null);
+            cursor.moveToFirst();
+            current_state = cursor.getInt(0);
+
+            personalAchievement.setProgress(((float)current_state/total_goal)*100);
+        }
     }
 }
